c1135679c259ecba980e71a8c206017aae9ec538
Serene Talia Namini <serene@Serenes-MacBook-Pro.local>
2016-11-09 16:01:50 -0800

    last

--- java.js
+++ java.js
@@ -20,52 +20,79 @@ $("#submit").on("click", function() {
 
 	// Code in the logic for storing and retrieving the train schedule.
 	// provide initial data to your Firebase database.
-
-	database.ref().push({
+	var thisobject = {
 		name: $('#nameinput').val().trim(),
 		destination: $('#destinationinput').val().trim(),
-		time: $('#timeinput').val().trim(),
 		frequency: $('#frequencyinput').val().trim(),
-	});
+		time: $('#timeinput').val().trim(),
+	}
+	console.log("thisobject***************",thisobject)
+
+	database.ref().push(thisobject);
 
 	// Don't refresh the page!
 	return false;
 });
 
+
 // listening to new children added to database
 	firebase.database().ref().on("child_added", function(snapshot){
+
+
+            var newTrainName = snapshot.val().train;
+            var newDestination = snapshot.val().destination;
+            var newFirstTrainTime = snapshot.val().trainTime;
+            var newFrequency = snapshot.val().frequency;
+
+            // Moment ============================================================
+
+
+		var tFrequency = snapshot.val().frequency;
+		var tFirstTrain = snapshot.val().time;
+		
+		// First Time (pushed back 1 year to make sure it comes before current time)
+		var firstTimeConverted = moment(tFirstTrain,"hh:mm").subtract(1, "years");
+		console.log(firstTimeConverted);
+
+		// Current Time
+		var currentTime = moment();
+		console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+		// Difference between the times
+		var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+		console.log("DIFFERENCE IN TIME: " + diffTime);
+
+		// Time apart (remainder)
+		var tRemainder = diffTime % tFrequency;
+		console.log(tRemainder);
+
+
+		// var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+		// nextTrainLLL = moment(nextTrain).format("hh:mm");
+		// console.log("ARRIVAL TIME: " + nextTrainLLL)
+		// console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
+
+
+		// Minute Until Train
+		var tMinutesTillTrain = tFrequency - tRemainder;
+		console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+// Next Train Arrival Time
+		var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+                console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
+
+		// 		// Next Train
+		// var nextTrain = currentTime + tMinutesTillTrain;
+		// // nextTrain = moment(nextTrain).format("hh:mm");
+		// // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
+
+
 		$("#train-data").append("<tr><td>" + snapshot.val().name +
 		 "</td>" + "<td>" + snapshot.val().destination + "</td>" +
-		  "<td>" + snapshot.val().frequency + "</td>" + "<td>" + 
-		  snapshot.val().time + "</td></tr>" );
+		  "<td>" + tFrequency + "</td>" 
+		  + "<td>" + nextTrain + "</td>"
+		  + "<td>" + tMinutesTillTrain + "</td></tr>" );
 
-	})
+	// })
 
-
-//Firebase watcher + initial loader
-database.ref().on("value", function(snapshot) {
-
-	// Log everything that's coming out of snapshot
-	console.log(snapshot.val());
-	console.log(snapshot.val().name);
-	console.log(snapshot.val().destination);
-	console.log(snapshot.val().time);
-	console.log(snapshot.val().frequency);
-
-
-// Handle the errors
-}, function(errorObject) {
-
-	console.log("Errors handled: " + errorObject.code);
-});
-
-
-var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-}
-
-$(window).scroll(function(e){
-    parallax();
-});
+	 }); // End database.ref().on('value', function(snapshot){
